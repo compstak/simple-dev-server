7d8f0b3155a8f0adbff99565e51c9f7e69124adf
Adam <ad@adamthomann.com>
2021-01-26 10:37:51 -0500

    Fix import statement

--- bin.js
+++ bin.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { forEach } from './index';
+import { forEach } from './index.js';
 
 forEach(function (devServer) {
 	console.log('starting dev server on port ' + devServer.get('port'));
