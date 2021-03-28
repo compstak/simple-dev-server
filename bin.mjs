1886edfb65e4a125b4883f202c0c1ce32ca3507f
Adam <ad@adamthomann.com>
2021-03-28 11:24:45 -0400

    Fix bin import

--- bin.mjs
+++ bin.mjs
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import { forEach } from "./index.mjs";
+import server from "./index.mjs";
 
-forEach(function (devServer) {
+server.forEach(function (devServer) {
   console.log("starting dev server on port " + devServer.get("port"));
   devServer.listen(devServer.get("port"));
 });
