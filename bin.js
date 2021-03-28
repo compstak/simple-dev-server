3f7933e67ce66f7d53438ab884fb542b0dbedcee
Adam <ad@adamthomann.com>
2021-03-28 11:03:30 -0400

    Fix bin.js

--- bin.js
+++ bin.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import server from './index.js';
+const server = require("./index.js");
 
 server.forEach(function (devServer) {
-	console.log('starting dev server on port ' + devServer.get('port'));
-	devServer.listen(devServer.get('port'));
+  console.log("starting dev server on port " + devServer.get("port"));
+  devServer.listen(devServer.get("port"));
 });
